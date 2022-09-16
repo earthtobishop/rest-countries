504c93fca4cf2c654636648cb8ff92c17ba09adc
Steven Bishop <stevenbishopdeveloper@gmail.com>
2022-09-16 15:53:58 -0700

    fetch countries on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Input from "../components/ui/Input";
 import Page from "../components/ui/Page";
+import { getCountries } from "../lib/countries";
 
-function HomePage() {
+export async function getStaticProps() {
+  const countries = await getCountries();
+  return {
+    props: {
+      countries
+    }
+  };
+}
+
+function HomePage({ countries }) {
+  console.log(countries);
   return (
     <Page title="Rest Countries">
       <Input />
